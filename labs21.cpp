825a062a05bfc1c3daec509c2964ce32684bc7d6
pluto <mackasto2005@gmail.com>
2025-03-19 12:04:10 +0100

    labs 2 zadaci

---
+++ labs21.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+//stack raboti taka sto posledniot clen koj sme go stavile prv go vadime
+struct stack{
+  int s[10],top;
+
+  void init(){ //se kreira stack top e -1 z.s. koga se koristi push top se zgolemuva za 1 i stanuva 0 so prviot clen
+    top=-1;
+  }
+  void overflow(){ //za koga ke se iskoristi push no nema veke mesto vo stackot
+    cout<<"Overflow"<<endl;
+    exit(-1);//ja prekinuva programata tuka i vraka -1
+  }
+  void underflow(){ //za koga ke se iskoristi pop no stackot e prazen
+    cout<<"Underflow"<<endl;
+    exit(-1);
+  }
+  void push(int n){ //se dodava clen vo stackot
+    if(top>=9)overflow(); //se proveruva dali ima mesto vo stackot
+    s[++top]=n;//se dodava clenot vo stackot, ++top z.s. top treba da se zgolemi prvo pa potoa da se izvrsi linijata
+               //a so top++ prvo se izvrsuva pa se zgolemuva
+  }
+  int pop(){//se trgna posledniot clen od stackot
+    if(top<0)underflow();//se proveruva dali e prazen stackot uslovot isto moze da bide (top==-1)
+    return s[top--];//se vraka clenot pa potoa se namaluva top
+  }
+  int peek() {//go gleda posledniot clen bez da se namali top
+    if(top<0)underflow();
+    return s[top];
+  }
+  bool iEmp() {
+    return top==-1;
+  }//proveruva dali e prazen stackot
+  bool iFul() {
+    return top==9;
+  }//proveruva dali e poln stackot, funkcijava ne ja koristam ama spomnata e vo zadacata :)
+};
+
+void funk(stack og) {
+  stack pom1,pom2;
+  int n;
+  pom1.init();
+  pom2.init();
+  while (!og.iEmp()) { //iEmp vrara false se dodeka ne se isprazni nizata
+    n=og.pop();//se zema clen od og se stava vo n i n se stava vo pom1 i pom2
+    pom1.push(n);
+    pom2.push(n);
+  }
+  while (!pom2.iEmp()) {
+    og.push(pom2.pop()); //z.s. so promenliva gi stavivme clenovite vo pom1 i pom2 clenovite imaat ist redosled
+    // a ni treba vo edna da se naopaku, od pom2 se zemaat i stavaat vo og
+  }
+  //taka se bara vo zadacata istovremeno da se stavat vo dvete pomosni inace ima polesen nacin :)
+  while (!pom1.iEmp()) { //se proveruva samo za eden stack z.s. imaat ist broj clenovi
+    if ( (pom1.peek()+og.peek()) >=9 ) { //se proveruva dali zbirot e pogolem ili ednakov na 9 ako e se pecati samo 9 i
+    //se trgaat clenovite od stackot
+      cout<<9;
+      pom1.pop();
+      og.pop();
+    }
+    else { //se pecati zbirot na clenovite so pop za da se trgnat
+      cout<<pom1.pop()+og.pop();
+    }
+  }
+}
+
+int main(){
+  stack og;
+  int n,n1;
+  og.init(); //se inicijalizija og
+  cout<<"daj broj gng(ne poishe od 10 cifri mame ti ebam sho ke ti se tolku cifri):"<<endl;
+  cin>>n;
+  while (n>0) { //gi zema broevite od kraj na brojot i gi stava vo og t.e. vo og se prevrteni broevite pr.
+    //ako n e 123 vo og se 321
+    n1=n%10;
+    n=n/10;
+    og.push(n1);
+  }
+  funk(og);
+}
---
+++ labs22.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+//queue raboti taka sto clenot koj sme go stavile prv go vadime prv
+struct queue{
+  int q[10],f,r;//f e indeks na prviot clen koj sme go stavile vo nizata a r e krajot na nizata
+  void overflow(){
+    cout<<"Overflow"<<endl;
+    exit(-1);
+  }
+  void underflow(){
+    cout<<"Underflow"<<endl;
+    exit(-1);
+  }
+  void init(){
+    f=r=-1;
+  }
+  void put(int n){
+    if(r>9)overflow();
+    else {
+      if(f==-1)f=0;//uslov za koga prv pat koristime put, togas da bide f=r=0
+      q[++r]=n;//prvo se zgolemuva r pa se dodava n
+    }
+  }
+  int pull(){
+    int n;
+    if(f==-1)underflow();
+    else{
+      n=q[f];
+      if(f==r)f=r=-1; //koga f=r toa znaci deka sme go izvadile posledniot clen na nizata i tuka taa se resetira
+      else f++;//dokolku ne e posleden clen indeksot na brojot koj e na red se zgolemuva z.s. sme izvadile eden
+    }
+    return n;
+  }
+  int peek(){
+    if(f==-1)underflow();
+    return q[f]; //ako ima clen vo nizata go gleda bez da se pomesti f
+  }
+};
+
+void funk(queue q, queue q1,int c) {
+  int n;
+  for (int i=0;i<c-1;i++) {
+    n=q.pull();//se zema cifrata koja prva e stavena vo q
+    if ( (n+q.peek())<=9 ) {//se proveruva dali zbirot na odredena cifra i slednata e pomal ili ednakov na 9
+      q1.put(n+q.peek());//ako e se stava vo q1, se pravi so peek a ne pull z.s. sporeduvanjeto ne e dve po dve cifri
+    }//tuku sekoja cifra se sporeduva so slednata pr. za 654721 6 so 5 se sporeduva pa 5 so 4 namesto 6 so 5 pa 4 so 7
+    else {
+      q1.put(n);//ako ne prvata cifra se stava vo q1
+    }
+  }
+  for (int i=0;i<c-1;i++) {
+    cout<<q1.pull(); //se pecati q1
+  }
+}
+
+int main(){
+  int n,n1,c=0,a[10];
+  queue q,q1;
+  q.init();
+  q1.init();
+  cout<<"daj broj gng(ne poishe od 10 cifri mame ti ebam sho ke ti se tolku cifri):"<<endl;
+  cin>>n;
+  for (int i=0;n>0;i++) {//vo niza se stavaat cifrite z.s. se naopaku a so redovi ne e mozno da se prevrti brojot
+    n1=n%10;
+    n=n/10;
+    a[i]=n1;
+    c++;
+  }
+  for (int i=0;i<c;i++) {//se prevrtuva brojot i se stava vo red
+    q.put(a[c-1-i]);
+  }
+  funk(q,q1,c);
+}
